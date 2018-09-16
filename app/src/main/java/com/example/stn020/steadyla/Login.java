a08c65f5518456651255eeb9fa93eb601a2fe110
Kelvin-ang <kelvinang94@gmail.com>
2018-09-16 20:29:27 +0800

    first commit

---
+++ app/src/main/java/com/example/stn020/steadyla/Login.java
@@ -0,0 +1,13 @@
+package com.example.stn020.steadyla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
